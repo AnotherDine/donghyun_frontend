41858702f8ffb9078ea43f25eafd982f3d4ae734
dh.kim <dh.kim@odinue.net>
2024-03-05 12:08:21 +0900

    [Chore] axios 주석제거

--- src/components/FirstBoot.vue
+++ src/components/FirstBoot.vue
@@ -29,30 +29,3 @@ export default {
     },
 }
 </script>
-<!--
-<script>
-import axios from 'axios';
-export default {
-    data() {
-        return {
-            dataList: [],
-        };
-    },
-
-    mounted() {
-        this.fetchData();
-    },
-
-    methods: {
-        async fetchData() {
-            try {
-                const response = await axios.get('http://ec2-3-36-216-183.ap-northeast-2.compute.amazonaws.com:8080/api/data');
-                this.dataList = response.data;
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        },
-    },
-}
-</script>
--->
