5ecb97a33475006dd76562d348fc3e0d42930bee
dh.kim <dh.kim@odinue.net>
2024-03-06 11:02:35 +0900

    [Style] ';' 추가

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import axios from './utils/axios';
 
-const app = createApp(App)
+const app = createApp(App);
+
 app.config.globalProperties.$http = axios;
 
-app.mount('#app')
+app.mount('#app');
